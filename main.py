467b2f4ebc49811a3654d3330713255caeb1f805
mayidadada <81606392+mayidadada@users.noreply.github.com>
2021-11-04 12:57:51 +0800

    Update main.py

--- main.py
+++ main.py
@@ -42,7 +42,26 @@ class dyxsplugin(StellarPlayer.IStellarPlayerPlugin):
         self.xls = []
         self.allmovidesdata = {}
         self.playurl = ''
-        urllib3.disable_warnings()
+        urllib3.disable_warnings()# -*- coding: utf-8 -*- 
+import StellarPlayer
+class Plugin(StellarPlayer.IStellarPlayerPlugin): 
+    def __init__(self, player:StellarPlayer.IStellarPlayer): 
+        super().__init__(player) 
+    
+    def handleRequest(self, method, args): 
+        if hasattr(self.simple, f"on_{method}"): 
+            getattr(self.simple, f"on_{method}")(args) 
+            
+    def start(self): 
+        print("插件启动") 
+        
+    def stop(self): 
+        super().stop() 
+        print("插件停止") 
+        
+    def newPlugin(player:StellarPlayer.IStellarPlayer,*arg): 
+        print("插件初始化") 
+        return Plugin(player) 
     
     def start(self):
         super().start()
